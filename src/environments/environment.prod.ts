b338f2a5a8250c399159c3adc1a4d1a964490e4f
Alex Nelson <alex.nelson.bryan@gmail.com>
2025-11-06 21:48:01 +0100

    chore: change prod api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Example: 'https://portfolio-backend.onrender.com/api/v1'
-  apiBaseUrl: 'https://correct-tillie-ninjashadowboy-b3e5aad0.koyeb.app/api/v1',
+  apiBaseUrl: 'https://kotlin-backend-apf5.onrender.com/api/v1',
 };
