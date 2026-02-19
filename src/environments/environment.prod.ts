7d35ae073b7ea0ddaba0cd6de1e050a0d7c4ec32
NinjaShadowBoy <alex.nelson.bryan@gmail.com>
2026-02-19 18:55:46 +0100

    fix: change backend url to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // Example: 'https://portfolio-backend.onrender.com/api/v1'
-  apiBaseUrl: 'portfolio-backend-production-4aa3.up.railway.app/api/v1',
+  apiBaseUrl: 'https://portfolio-backend-production-4aa3.up.railway.app/api/v1',
   cloudinary: {
     cloudName: 'dct6fuenh',
     uploadPreset: 'portfolio_unsigned', // Create this in Cloudinary dashboard
