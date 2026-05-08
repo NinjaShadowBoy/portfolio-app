af9b8c43af041b3464909d23b7c54af308b32c84
Abena Alex <alex.nelson.bryan@gmail.com>
2026-05-08 07:15:53 +0100

    chore: Change api url to aws vps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // Example: 'https://portfolio-backend.onrender.com/api/v1'
-  apiBaseUrl: 'https://portfolio-backend-production-4aa3.up.railway.app/api/v1',
+  // VPS backend with /portfolio prefix
+  apiBaseUrl: 'https://ec2-13-51-63-171.eu-north-1.compute.amazonaws.com/portfolio/api/v1',
   cloudinary: {
     cloudName: 'dct6fuenh',
     uploadPreset: 'portfolio_unsigned', // Create this in Cloudinary dashboard
