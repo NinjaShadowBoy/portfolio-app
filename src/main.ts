58a78c2cfecec99d81d9bf6a8fd2163ad623bb77
alex-abena <alex.nelson.bryan@gmail.com>
2026-07-18 12:52:21 +0200

    fix: Reference jasmine types

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 /// <reference types="@angular/localize" />
+/// <reference types="jasmine" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
